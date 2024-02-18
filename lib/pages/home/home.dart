b962f8d2be3aae9ccdf56d649b58813a7dd87acb
Sam Lakerveld <dark@dark.red>
2024-02-18 23:53:40 +0100

    minimal search

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:osm_nominatim/osm_nominatim.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import 'subs/search.dart';
@@ -11,6 +11,28 @@ part 'home.g.dart';
 
 @Riverpod(keepAlive: true)
 MapController mapController(Ref ref) => MapController();
+
+StateProvider<String> searchQueryProvider = StateProvider((ref) => '');
+
+@riverpod
+Future<List<Place>> searchResults(Ref ref) async {
+  final query = ref.watch(searchQueryProvider);
+
+  if (query.isEmpty) {
+    return [];
+  }
+
+  final bounds = ref.read(mapControllerProvider).camera.visibleBounds;
+
+  return await Nominatim.searchByName(
+    query: query,
+    viewBox: ViewBox(bounds.north, bounds.south, bounds.east, bounds.west),
+    limit: 20,
+    // addressDetails: true,
+    // extraTags: true,
+    // nameDetails: true,
+  );
+}
 
 class TheMap extends ConsumerStatefulWidget {
   const TheMap({
@@ -32,12 +54,15 @@ class _TheMapState extends ConsumerState<TheMap> {
 
   @override
   Widget build(BuildContext context) {
+    final results = ref.watch(searchResultsProvider).valueOrNull ?? [];
+    print(results);
+
     return Scaffold(
       body: FlutterMap(
         mapController: ref.watch(mapControllerProvider),
         options: const MapOptions(
           initialCenter: LatLng(35.699680629055365, 139.77137788132293),
-          initialZoom: 12,
+          initialZoom: 18,
           maxZoom: 18,
           interactionOptions: InteractionOptions(
             flags: InteractiveFlag.all & ~InteractiveFlag.rotate,
@@ -48,7 +73,16 @@ class _TheMapState extends ConsumerState<TheMap> {
             urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
             maxZoom: 18,
             maxNativeZoom: 18,
-            retinaMode: RetinaMode.isHighDensity(context),
+            // retinaMode: RetinaMode.isHighDensity(context),
+          ),
+          MarkerLayer(
+            markers: [
+              for (final place in results)
+                Marker(
+                  point: LatLng(place.lat, place.lon),
+                  child: Icon(Icons.place, color: Colors.red),
+                ),
+            ],
           ),
         ],
       ),
@@ -57,12 +91,16 @@ class _TheMapState extends ConsumerState<TheMap> {
           : subLocation == SubLocation.search
               ? const SearchSheet()
               : null,
-      floatingActionButton: subLocation == SubLocation.none
+      floatingActionButton: subLocation == SubLocation.search
           ? null
           : FloatingActionButton(
               child: const Icon(Icons.search),
               onPressed: () {
-                context.go('/search');
+                ref.watch(searchQueryProvider.notifier).state = "gigo";
+
+                // setState(() {
+                //   subLocation = SubLocation.search;
+                // });
               },
             ),
     );
--- lib/pages/home/home.g.dart
+++ lib/pages/home/home.g.dart
@@ -21,5 +21,20 @@ final mapControllerProvider = Provider<MapController>.internal(
 );
 
 typedef MapControllerRef = ProviderRef<MapController>;
+String _$searchResultsHash() => r'954e1b7dee1252a655888afcdcf374dc547f35de';
+
+/// See also [searchResults].
+@ProviderFor(searchResults)
+final searchResultsProvider = AutoDisposeFutureProvider<List<Place>>.internal(
+  searchResults,
+  name: r'searchResultsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$searchResultsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef SearchResultsRef = AutoDisposeFutureProviderRef<List<Place>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
